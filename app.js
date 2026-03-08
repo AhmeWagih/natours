89490201b778b15efcfbdcaab0247f0a61fbf8c1
AhmeWagih <ahmedwagih087@gmail.com>
2026-03-08 17:09:43 +0200

    implements the view controllers and rout and also the view pugs fils

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
@@ -5,16 +6,22 @@ const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 const hpp = require("hpp");
-const app = express();
 
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
-
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
+const viewRouter = require("./routes/viewRoutes");
 
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+// Global middleware
+// Serving static files
+app.use(express.static(path.join(__dirname, "public")));
 // Middleware
 // Set security HTTP headers
 app.use(helmet());
@@ -46,8 +53,6 @@ app.use(xss());
 app.use(hpp({
   whitelist: ["duration", "ratingsQuantity", "ratingsAverage", "maxGroupSize", "difficulty", "price"]
 }));
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
 
 // Test middleware
 app.use((req, res, next) => {
@@ -56,6 +61,9 @@ app.use((req, res, next) => {
 });
 
 // Routes
+
+
+app.use("/", viewRouter);
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
